3e6a2ea31853fa1f1dd9e614f5d000cfc01f2069
Chad Cottle <cottle.chad@yahoo.com>
2019-12-13 14:47:04 +0000

    new startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 yum update -y
 yum install httpd -y
-yum install mysql-server mysql -y
-systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
+service httpd start
+chkconfig httpd on
+yum install wget -y
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+yum install mysql-server -y
+systemctl start mysqld
 chkconfig mysqld on
 service mysqld start
 yum install git -y
 yum install php php-mysqlnd php-pdo php-gd php-mbstring php-dom -y
 cd /var/www/html
 git clone https://github.com/GetDKAN/dkan-drops-7.git
-echo “<? \n phpinfo(); \n phpinfo(INFO_MODULES); \n ?>” >> /var/www/html/phpinfo.php
